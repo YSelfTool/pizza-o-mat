327ce9dbcb8d0bf57301e1abca2c3dd4aaf4c767
Robin Sonnabend <robin@fsmpi.rwth-aachen.de>
2017-09-06 22:51:45 +0200

    Fit file structure to flask

--- templates/app.js
+++ templates/app.js
@@ -354,7 +354,7 @@ var studimat = function() {
     };
 
     // TODO: this should be configurable
-    httpRequest.open('GET', "data/demo_"+self.wahlomat_language+".json");
+    httpRequest.open('GET', "data/{{restaurant_id}}_"+self.wahlomat_language+".json");
     httpRequest.send();
   }
 
